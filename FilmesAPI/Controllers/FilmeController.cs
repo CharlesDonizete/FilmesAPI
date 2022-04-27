8140373a322c20a105392a7c0c29b6f94831b927
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2022-04-27 11:18:34 -0300

    Criação da CreatedAtAction para mandar de volta para quem fez a requisição o caminho onde pode ser acessado o novo filme cadastrado.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -15,22 +15,29 @@ namespace FilmesAPI.Controllers
         private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
             filme.Id = id++;
             filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperarFilmesPorId), new {Id = filme.Id }, filme);
+        }        
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
         }
 
         [HttpGet("{id}")]
-        public Filme RecuperarFilmesPorId(int id)
+        public IActionResult RecuperarFilmesPorId(int id)
         {
-            return filmes.FirstOrDefault(filme => filme.Id == id);
-        }
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
 
-        [HttpGet]
-        public IEnumerable<Filme> RecuperaFilmes()
-        {
-            return filmes;
-        }        
+        }
     }
 }
