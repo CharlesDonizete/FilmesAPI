cdea6cdf249acbf7729dda1e02a8bfdf5f55c944
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2022-04-27 10:42:08 -0300

    Feito a recuperação da lista de filmes com verbo get.

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -8,6 +8,7 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
         [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
@@ -15,6 +16,6 @@ namespace FilmesAPI.Models
         [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
         public string Genero { get; set; }
         [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
-        public int Duracao { get; set; }
+        public int Duracao { get; set; }        
     }
 }
