f99754713a5b017990a8f28b61a02ca353b8db44
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2022-05-03 11:24:38 -0300

    Relacionamento Endereço e Cinema

--- FilmesAPI/Models/Endereco.cs
+++ FilmesAPI/Models/Endereco.cs
@@ -13,6 +13,7 @@ namespace FilmesAPI.Models
         public int Id { get; set; }
         public string Logradouro { get; set; }
         public string Bairro { get; set; }
-        public int Numero { get; set; }        
+        public int Numero { get; set; }
+        public Cinema Cinema { get; set; }
     }
 }
