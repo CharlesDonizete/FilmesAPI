5428f43476606e5b6ac9777cf0d2cd4753321429
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2022-05-11 10:41:38 -0300

    Relacionamento n:n - Criando a entidade Sessão.

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -19,6 +19,6 @@ namespace FilmesAPI.Models
         public string Genero { get; set; }
         [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
-        public virtual List<Cinema> Cinemas { get; set; }
+        public virtual List<Sessao> Sessoes { get; set; }
     }
 }
