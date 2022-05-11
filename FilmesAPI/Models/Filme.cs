8e44f4f4bdf02fd3bc087cea295b68d588a53490
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2022-05-11 10:15:45 -0300

    Relacionamento n:n - Relacionamento Filme e Cinema

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -18,6 +18,7 @@ namespace FilmesAPI.Models
         [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
         public string Genero { get; set; }
         [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
-        public int Duracao { get; set; }        
+        public int Duracao { get; set; }
+        public virtual List<Cinema> Cinemas { get; set; }
     }
 }
