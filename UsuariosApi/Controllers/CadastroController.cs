28517445f98810797b182ab192bcccd733515088
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2022-05-17 17:19:19 -0300

    02. Conhecendo o Identity - 04. Recebendo um usuário.

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastrarUsuario(CreateUsuarioDto createDto)
+        {
+            //TODO chamar o service
+            return Ok();
+        }
+    }
+}
