e725c89989b16fe0c8753679aa1c3328e001e2f5
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2022-05-26 11:06:05 -0300

    03. Logando um suário - 04. Gerando o token

--- UsuariosApi/Services/LoginService.cs
+++ UsuariosApi/Services/LoginService.cs
@@ -11,16 +11,23 @@ namespace UsuariosApi.Services
     public class LoginService
     {
         private SignInManager<IdentityUser<int>> _signInManager;
+        private TokenService _tokenService;
 
-        public LoginService(SignInManager<IdentityUser<int>> signInManager)
+        public LoginService(SignInManager<IdentityUser<int>> signInManager, TokenService tokenService = null)
         {
             _signInManager = signInManager;
+            _tokenService = tokenService;
         }
 
         public Result LogaUsuario(LoginRequest request)
         {
             var resultadoIdentity = _signInManager.PasswordSignInAsync(request.UserName, request.Password, false, false);
-            if (resultadoIdentity.Result.Succeeded) return Result.Ok();
+            if (resultadoIdentity.Result.Succeeded)
+            {
+                //_tokenService.CreateToken()
+                return Result.Ok();
+            }
+                
             return Result.Fail("Login falhou");
         }
     }
